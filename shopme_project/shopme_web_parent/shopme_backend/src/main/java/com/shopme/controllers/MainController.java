2aa77dfd96af9992be5e58a95c5a4aa429a7f016
Alphanumeric Str <150432237+numericar@users.noreply.github.com>
2025-02-16 13:35:03 +0700

    initial multi module project

---
+++ shopme_project/shopme_web_parent/shopme_backend/src/main/java/com/shopme/controllers/MainController.java
@@ -0,0 +1,16 @@
+package com.shopme.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("")
+public class MainController {
+
+    @GetMapping
+    public String viewHomePage() {
+        return "index";
+    }
+
+}

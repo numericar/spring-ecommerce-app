829d82e29cad87ffda52d1b0130d326fb4308fc8
Alphanumeric Str <150432237+numericar@users.noreply.github.com>
2025-02-19 12:11:35 +0700

    validate email is dupplicate

---
+++ shopme_project/shopme_web_parent/shopme_backend/src/main/java/com/shopme/controllers/UserRestController.java
@@ -0,0 +1,26 @@
+package com.shopme.controllers;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.shopme.services.UserService;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserRestController {
+
+    private final UserService userService;
+
+    public UserRestController(UserService userService) {
+        this.userService = userService;
+    }
+
+    // @Param ใช้ในการรับค่า parameter ที่ส่งมาจาก client ผ่าน URL เช่น /check_email?email=xxx
+    @PostMapping("/check_email_unique")
+    public String checkEmailUnique(@RequestParam("email") String email) {
+        return this.userService.isEmailUnique(email) ? "OK" : "Duplicated";
+    }
+
+}
